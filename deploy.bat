d46cde500b73105d50b74fd5a11fafdfa8f6b9df
Fausto G. Cintra <fausto@fgcintra.pro.br>
2019-09-02 22:15:22 -0300

    Scripts de deploy

---
+++ deploy.bat
@@ -0,0 +1,30 @@
+"%USERPROFILE%\AppData\Roaming\npm\ng.cmd" build --base-href /pinupload/ && ^
+git add -f dist && ^
+git commit -m %1 && ^
+git subtree push --prefix=dist/pinupload origin gh-pages
+
+REM Compila o projeto Angular para deploy.
+REM Este comando gera ou atualiza a pasta /dist
+REM "%USERPROFILE%\AppData\Roaming\npm\ng.cmd" build --base-href /pinupload/
+
+REM Adicionar a pasta /dist para ser enviada ao GitHub.
+REM A opção -f força a adição mesmo se o caminho estiver
+REM configurado no arquivo .gitignore
+REM SOMENTE SERÁ EXECUTADO SE O ANTERIOR NÃO RETORNAR ERRO
+REM (ERRORLEVEL 0)
+REM IF %ERRORLEVEL% EQU 0 (
+REM   git add -f dist
+REM)
+
+REM Commit com mensagem. %1 será substituído pela mensagem
+REM IF %ERRORLEVEL% EQU 0 (
+REM   git commit -m %1
+REM)
+
+REM Envio dos arquivos para o servidor. Vamos enviar apenas
+REM os arquivos debaixo de /dist (subtree), ignorando o caminho
+REM /dist/pinupload no caminho do repositório. Os arquivos enviados
+REM sairão localmente do branch origin para o branch gh-pages remoto
+REM IF %ERRORLEVEL% EQU 0 (
+REM   git subtree push --prefix=/dist/pinupload origin gh-pages
+REM )
---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Compila o projeto Angular para deploy. Caso você esteja
+# utilizando o git bash, utilize duas barras // após --base-href.7
+# Este comando gera ou atualiza a pasta /dist
+ng build --base-href /pinupload/
+
+# Adicionar a pasta /dist para ser enviada ao GitHub.
+# A opção -f força a adição mesmo se o caminho estiver
+# configurado no arquivo .gitignore
+git add -f dist
+
+# Commit com mensagem
+git commit -m "$1"
+
+# Envio dos arquivos para o servidor. Vamos enviar apenas
+# os arquivos debaixo de /dist (subtree), ignorando o caminho
+# /dist/pinupload no caminho do repositório. Os arquivos enviados
+# sairão localmente do branch origin para o branch gh-pages remoto
+git subtree push --prefix=dist/pinupload origin gh-pages
