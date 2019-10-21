0c23105489043fe0830638e4ed397d13567c5f8b
Fausto G. Cintra <fausto@fgcintra.pro.br>
2019-10-21 18:14:12 -0300

    back again 28

--- dist/pinupload/main-es5.js
+++ dist/pinupload/main-es5.js
@@ -662,7 +662,7 @@ var PinterestService = /** @class */ (function () {
         var params = new _angular_common_http__WEBPACK_IMPORTED_MODULE_2__["HttpParams"]()
             .set('access_token', this.accessToken)
             .set('scope', 'read_public');
-        this.http.get('https://crossorigin.me/' + this.env.apiUri + endPoint, { params: params }).subscribe(function (result) {
+        this.http.get(this.env.apiUri + endPoint, { params: params }).subscribe(function (result) {
             if (result) {
                 console.log(result);
             }
