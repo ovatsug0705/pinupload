1a462cd7f56e0becc9cc6c0e2402b0de7e2381e8
Fausto G. Cintra <fausto@fgcintra.pro.br>
2019-10-21 18:22:32 -0300

    back again 29

--- main-es2015.js
+++ main-es2015.js
@@ -647,7 +647,9 @@ let PinterestService = class PinterestService {
         const params = new _angular_common_http__WEBPACK_IMPORTED_MODULE_2__["HttpParams"]()
             .set('access_token', this.accessToken)
             .set('scope', 'read_public');
-        this.http.get(this.env.apiUri + endPoint, { params: params }).subscribe(result => {
+        const headers = new _angular_common_http__WEBPACK_IMPORTED_MODULE_2__["HttpHeaders"]()
+            .set('Access-Control-Allow-Origin', 'https://faustocintra.github.io');
+        this.http.get(this.env.apiUri + endPoint, { params: params, headers: headers }).subscribe(result => {
             if (result) {
                 console.log(result);
             }
--- main-es5.js
+++ main-es5.js
@@ -662,7 +662,9 @@ var PinterestService = /** @class */ (function () {
         var params = new _angular_common_http__WEBPACK_IMPORTED_MODULE_2__["HttpParams"]()
             .set('access_token', this.accessToken)
             .set('scope', 'read_public');
-        this.http.get(this.env.apiUri + endPoint, { params: params }).subscribe(function (result) {
+        var headers = new _angular_common_http__WEBPACK_IMPORTED_MODULE_2__["HttpHeaders"]()
+            .set('Access-Control-Allow-Origin', 'https://faustocintra.github.io');
+        this.http.get(this.env.apiUri + endPoint, { params: params, headers: headers }).subscribe(function (result) {
             if (result) {
                 console.log(result);
             }
